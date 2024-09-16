83ff528bd5d949c24e28814fdf04a4b1c53081f6
Nataliia <pnata1978@gmail.com>
2024-09-16 20:58:56 +0200

    new project-3

--- src/test/java/org/qa/phonebook/HomePageTest.java
+++ src/test/java/org/qa/phonebook/HomePageTest.java
@@ -1,12 +1,15 @@
 package org.qa.phonebook;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 public class HomePageTest {
@@ -22,8 +25,31 @@ public class HomePageTest {
 
     @Test
     public void isHomeComponentPresentTest(){
-        driver.findElement(By.)
+       //System.out.println("Home Component is "+ isHomeComponentPresent());
+//       System.out.println("Home Component is "+
+//               isElementPresent(By.xpath("//h1[text()='Home Component']")));
+
+        Assert.assertTrue(isHomeComponentPresent());
     }
+
+    public boolean isHomeComponentPresent(){
+        return driver.findElements(By.xpath("//h1[text()='Home Component']")).size()>0;
+    }
+
+    public boolean isElementPresent(By locator){
+        return driver.findElements(locator).size()>0;
+    }
+
+
+    public boolean  isElementPresent2(By locator){
+        try {
+            driver.findElement(locator);
+            return true;
+        }catch (NoSuchElementException ex){
+            return false;
+        }
+    }
+
 
 
     @AfterMethod
