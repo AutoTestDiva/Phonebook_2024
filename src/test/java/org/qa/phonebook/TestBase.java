5d8ed2dd1c8d4a95dd195b0c2c0729f66727b911
Nataliia <pnata1978@gmail.com>
2024-09-21 20:19:36 +0200

    new project-15

--- src/test/java/org/qa/phonebook/TestBase.java
+++ src/test/java/org/qa/phonebook/TestBase.java
@@ -1,20 +1,34 @@
 package org.qa.phonebook;
 
 import org.qa.phonebook.frameWork.ApplicationManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", "chrome"));
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
         app.init();
     }
 
     @AfterMethod //(enabled = false)
-    public void  tearDown(){
+    public void tearDown() {
         app.stop();
+    }
+
+
+    @BeforeMethod
+    public void startTest() {
+    logger.info("Start test");
+    }
+
+    @AfterMethod
+    public void stopTest(){
+        logger.info("Stop test");
     }
 }
