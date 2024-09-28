b1f6910b7ffdb2d5068065ff7018f105ccdbf6af
Nataliia <pnata1978@gmail.com>
2024-09-28 17:59:47 +0200

    changes-3

--- src/test/java/org/qa/phonebook/AddContactTests.java
+++ src/test/java/org/qa/phonebook/AddContactTests.java
@@ -1,20 +1,11 @@
 package org.qa.phonebook;
 
 import org.qa.phonebook.models.Contact;
+import org.qa.phonebook.utils.DataProviders;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 public class AddContactTests extends TestBase {
     @BeforeMethod
@@ -45,35 +36,7 @@ public class AddContactTests extends TestBase {
         app.getContact().removeContact();
     }
 
-    @DataProvider
-    public Iterator<Object[]> newContact(){
-        List<Object[]> list = new ArrayList<>();
-        list.add(new Object[]{"Oliver", "Kan", "1234567890", "kan@gmail.com", "Berlin", "actor"});
-        list.add(new Object[]{"Oliver1", "Kan", "1234567898", "kan@gmail.com", "Berlin", "actor"});
-        list.add(new Object[]{"Oliver2", "Kan", "1234567899", "kan@gmail.com", "Berlin", "actor"});
-        return list.iterator();
-    }
-    @DataProvider
-    public Iterator<Object[]> newContactWithCSVFile() throws IOException {
-        List<Object[]> list = new ArrayList<>();
-        BufferedReader reader= new BufferedReader(new FileReader(new File("src/test/resources/contact.csv")));
-        String line = reader.readLine();
-
-        while (line != null) {
-            String[] split = line.split(",");
-
-            list.add(new Object[]{new Contact()
-                    .setName(split[0])
-                    .setSurname(split[1])
-                    .setPhone(split[2])
-                    .setEmail(split[3])
-                    .setAddress(split[4])
-                    .setDescription(split[5])});
-        line = reader.readLine();
-        }
-        return list.iterator();
-    }
-    @Test(dataProvider = "newContact")
+      @Test(dataProvider = "newContact", dataProviderClass = DataProviders.class)
     public void addContactPositiveTestFromDataProvider(String name, String surname, String phone,
                                                        String email, String address,String description) {
         app.getContact().fillContactForm(new Contact()
@@ -88,7 +51,7 @@ public class AddContactTests extends TestBase {
         Assert.assertTrue(app.getContact().isContactAdded(name));
     }
 
-    @Test(dataProvider = "newContactWithCSVFile")
+    @Test(dataProvider = "newContactWithCSVFile", dataProviderClass = DataProviders.class)
     public void addContactPositiveTestFromDataProviderWithCSV(Contact contact) {
         app.getContact().fillContactForm(contact);
         app.getContact().pause(1000);
